2a45f910c23c8e4323157c9a7fcceebfbf61c6c5
LAPTOP-PBIADF1K\krish <kashyap@ezapi.ai>
2023-01-29 02:50:19 +0530

    chng to path

--- src/main/java/com/org/gen/json2pojo/model/DTOGenDriver.java
+++ src/main/java/com/org/gen/json2pojo/model/DTOGenDriver.java
@@ -189,7 +189,7 @@ public class DTOGenDriver {
                     //String projBasePath = "/src/main/java/com/ezapi/api/";
                     respMsg=runCommand("sh", "-c", "rm -rf " + " /mnt/codegen/"+projectid+prgrmType+projBasePath+"*JHipster.java");
                     logger.info("respMsg.."+respMsg);                    
-					String fileGenerated = runCommand("sh", "/c", "ls *.java | tr '\n' '\n' " + currdir + outputPojoDirectory.getPath().toString()+"/"+pkgPath);
+					String fileGenerated = runCommand("sh", "/c", "ls *.java | tr '\n' '\n' " + outputPojoDirectory.getPath().toString()+"/"+pkgPath);
 					logger.info("fileGenerated.."+fileGenerated);
                     //runCommand("sh", "-c", "mv " +currdir+"/"+outputPojoDirectory.getPath().toString()+"/"+pkgPath+"/*"+ " /mnt/codegen/"+projectid+prgrmType+projPath);
 					runCommand("sh", "-c", "mv " +outputPojoDirectory.getPath().toString()+"/"+pkgPath+"/*"+ " /mnt/codegen/"+projectid+prgrmType+projPath);
